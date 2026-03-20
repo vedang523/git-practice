eb7dfb8e1ee87026541bbd574d3196fb7fa598ea
Vedang <vedangpro6999@gmail.com>
2026-03-20 12:13:06 +0530

    Renamed the hello file as main.py

---
+++ python_code/main.py
@@ -0,0 +1,18 @@
+def first_program():
+    for i in range(0,11):
+        print(i)
+
+first_program()
+
+def even_odd():
+  num =  int(input("Enter a number: "))
+
+  if num % 2 ==0:
+     print(f"{num} is a even number")
+  else:
+     print(f"{num} is a odd number")
+
+
+
+  
+even_odd()
