25c5771974c35220269d30f6ea639cef1f7f8206
Vedang <vedangpro6999@gmail.com>
2026-03-20 11:01:15 +0530

    Added a new feature to code of even_odd

--- hello.py
+++ hello.py
@@ -3,3 +3,13 @@ def first_program():
         print(i)
 
 first_program()
+
+def even_odd():
+    num = int(input("Enter a Number: "))
+
+    if num % 2 == 0:
+        print(f"{num} is an even number")
+    else:
+        print(f"{num} is an odd Number")
+
+even_odd()
